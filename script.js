d0a07e63eaa6d942bc054a4125aafb5b8fc281f4
darialiit <solomkina.daria@gmail.com>
2023-08-08 07:20:24 -0300

    add basic culculations

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
-const numbers = document.querySelectorAll(".number");
+const numbers = document.querySelectorAll(".number"); // needed? 
 const operators = document.querySelectorAll(".operator");
+const keys = document.querySelectorAll(".key");
+const result = document.querySelector(".result");
 
-let num1;
-let num2;
+let num1 = "";
+let num2 = "";
 let opr;
+let continueLoop1 = true;
+let continueLoop2 = true;
 
-// Listen to the operator chosen
+
+// Stop remembering the first number when the operator is pressed
 operators.forEach(operator => {
     operator.addEventListener("click", (e) => {
-        // console.log(operator.textContent);
-        opr = operator.textContent;
-
+        continueLoop1 = false;
+        opr = operator.innerHTML;
     });
 });
 
-// listen to the numeber chosen 
-numbers.forEach(number => {
-    number.addEventListener("click", (e) => {
-        // console.log(number.textContent)
-        num1 = number.textContent;
+// Stop remembering the second number when the = is pressed
+result.addEventListener("click", (e) => {
+    continueLoop2 = false;
+});
+
+// Function to unity everything WIP
+keys.forEach(key => {
+    key.addEventListener("click", (e) => {
+        if (continueLoop1 == true) {
+            num1 += key.innerHTML;
+            console.log(num1);
+        } else if (continueLoop2 == true) {
+            if(!key.className.includes("operator")) {
+                num2 += key.innerHTML;
+                console.log(num2);
+            };
+        } else {
+            console.log(operate(num1, num2, opr));
+        }
     });
 });
+
+
 
 // operator function
-
 function operate(numberOne, numberTwo, operation) {
     if (operation == "+") {
         return +numberOne + +numberTwo;
@@ -36,7 +55,9 @@ function operate(numberOne, numberTwo, operation) {
     };
 };
 
-// console.log(operate("7", "3", "+"))
 
 // function to populate the display number buttons are clicked
-
+// const display = document.querySelector("#screen");
+// function updateDisplay(one) {
+//     display.textContent += one;
+// }
